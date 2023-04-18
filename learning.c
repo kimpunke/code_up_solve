074d2502fcd3e2ace2a4a05eb60b38b4b9991368
kimpunke <kts300005@gmail.com>
2023-04-18 10:45:49 +0900

    problem 1080 try 4

--- learning.c
+++ learning.c
@@ -3,14 +3,13 @@
 int main(){
 
     int a,b,c;
+    c=0;
     scanf("%d", &a);
     for(b=1;c>=a;b++){
-        if(c<=b){
-            c+=b;
-        }
+        c+=b;
         
     }
-    printf("%d", a);
+    printf("%d", b);
 
     return 0;
 }
