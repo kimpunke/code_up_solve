d5019142796fbb1df75caa4d1da95ae4893ba35f
kimpunke <kts300005@gmail.com>
2023-04-18 09:09:45 +0900

    problem 1072-while

--- learning.c
+++ learning.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    int n;
-    for(;;){
+    int n, v;
+    scanf("%d", &v);
+    while(1){
         scanf("%d", &n);
-        
-        if(n==0) break; 
-        else printf("%d\n", n);
+        printf("%d\n", n);
+        v-=1;
+        if (v==0){
+            break;
+        }
     }
-
     return 0;
-    
 }
