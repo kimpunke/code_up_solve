9b01a78f159828be5fb139400ca61b2dff7fc90f
kimpunke <kts300005@gmail.com>
2023-04-18 10:06:40 +0900

    problem 1084 try 2

--- learning.c
+++ learning.c
@@ -4,13 +4,14 @@ int main(){
     int r,g,b;
     int i,j,k;
     scanf("%d %d %d", &r, &g, &b);
-    for(i=0;i<=r;i++){
-        for(j=0;j<=g;j++){
-            for(k=0;k<=b;k++){
+    for(i=0;i<r;i++){
+        for(j=0;j<g;j++){
+            for(k=0;k<b;k++){
                 printf("%d %d %d\n", i, j, k);
             }
         }
     }
+    printf("%d",r*g*b);
     
     return 0;
 }
