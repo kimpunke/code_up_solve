f378d464022450c107bfe2085accb5bad985d6fa
kimpunke <kts300005@gmail.com>
2023-04-19 10:13:04 +0900

    problem 1090 try 12

--- learning.c
+++ learning.c
@@ -4,8 +4,8 @@ int main(){
 
     int a,b,c,i;
     scanf("%d %d %d", &a, &b, &c);
-    
-    for(i=0; i <= c; i++){
+    c-=1;
+    for(i=0; i < c; i++){
         a=a*b;
     }
     printf("%d", a);
