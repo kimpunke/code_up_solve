78f669bade63c035611e4edd22f99fe86975e9b1
kimpunke <kts300005@gmail.com>
2023-04-19 10:25:03 +0900

    problem 1090 try 16(answer)

--- learning.c
+++ learning.c
@@ -2,13 +2,13 @@
 
 int main(){
 
-    int a,b,c,i=0;
+    long long int a,b,c,i=0;
     scanf("%d %d %d", &a, &b, &c);
     for(i=1; i < c; i++){
-        a=a*b;
+        a = a*b;
     }
 
-    printf("%d", a);
+    printf("%lld", a);
 
     return 0;
 }
