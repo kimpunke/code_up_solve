b41ef8950aadd902060f152cb32cb3ebfd8cbde3
kimpunke <kts300005@gmail.com>
2023-04-20 12:12:21 +0900

    problem 1253 try 1-infinite roof (answer)

--- learning.c
+++ learning.c
@@ -2,9 +2,38 @@
 
 int main(){
 
-    int n;
-    scanf("%d", &n);
-    printf("%d", (n*(n+1))/2);
+    int a,b;
+    scanf("%d %d", &a, &b);
+    while(1)
+    {
+        if(a>b)
+        {
+            if(a==b)
+        {
+            printf("%d ", b);
+            break;
+        }
+        else
+        {
+            printf("%d ", b);
+            b+=1;
+        }
+        }
+        else
+        {
+            if(a==b)
+            {
+                printf("%d ", a);
+                break;
+            }
+            else
+            {
+                printf("%d ", a);
+                a+=1;
+            }
+        }
+
+    }
 
     return 0;
 }
