cc7d68a7690c1e323bd1b5580bf5cb94892c0610
kimpunke <kts300005@gmail.com>
2023-07-20 22:18:06 +0900

    problem 1355 try1 (expression error)

--- learning.c
+++ learning.c
@@ -4,11 +4,14 @@ int main()
 {
    int i, j, n;
    scanf( "%d", &n );
-   for( i = n ; i >= 1 ; i-- )
-   {
-      for( j = 1 ; j <= i ; j++ )
-         printf( "*" );
-      printf("\n");   
+   for(i=1; i<=n; i++){
+      for(j=i; j>=0; j--){
+         printf(" ");
+      }
+      for(j=i; j<=n; j++){
+         printf("*");
+      }
+      printf("\n");
    }
    return 0;
 }
