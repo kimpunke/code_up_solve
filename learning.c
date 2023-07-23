cb5d784c44d0aec1d206cb510e3fded25f24547c
kimpunke <kts300005@gmail.com>
2023-07-23 17:27:48 +0900

    problem 1362 try2 (answer)

--- learning.c
+++ learning.c
@@ -11,7 +11,7 @@ int main (){
     scanf("%d", &n);
     count = f(n);
     for(i=1; i<=n; i++){
-      for(j=1; j<=n; j++){
+      for(j=1; j<=i; j++){
          printf("%d ", count);
          count--;
       }
