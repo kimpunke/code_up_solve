b6af4a6bea7cf19ec81e83831c6ab9c8cd2b1ed2
kimpunke <kts300005@gmail.com>
2023-07-23 17:18:13 +0900

    problem 1359 try 1 (answer)

--- learning.c
+++ learning.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
-   int i, j, n;
-   scanf( "%d", &n );
-   for (int i = 1; i <= n; i += 2) {
-        for (int j = 0; j < (n - i) / 2; j++) {
-            printf(" ");
-        }
-        for (int j = 0; j < i; j++) {
-            printf("*");
+
+int main(){
+    int n,i,j,count;
+    scanf("%d",&n);
+    for(i=1; i<=n; i++){
+        count=1;
+        for(j=n-i; j<n; j++){
+            printf("%d ",count);
+            count++;
         }
         printf("\n");
     }
-   return 0;
+    
+    
+    
+    
+    
+    return 0;
+    
+    
 }
