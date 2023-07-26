0b01e16051ba8596d1d557cbba12543ab927a626
punke <kts300005@gmail.com>
2023-07-26 19:43:53 +0900

    problem 1358 try 5

--- learning.c
+++ learning.c
@@ -4,11 +4,11 @@ int main()
 {
    int i, j, n;
    scanf( "%d", &n );
-   for(i=1;i<=n-2; i++){
-      for(j=1; j<=n-i-1;j++){
+   for(i=0;i<n-2; i++){
+      for(j=0; j<n-i-1;j++){
          printf(" ");
       }
-      for(j=1; j<=(i*2)+1; j++){
+      for(j=0; j<(i*2)+1; j++){
          printf("*");
       }
       printf("\n");
