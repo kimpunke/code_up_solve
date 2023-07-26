46984a37ab4d9e449eda5d35f93b48b19001e20d
punke <kts300005@gmail.com>
2023-07-26 19:40:40 +0900

    problem 1358 try 2

--- learning.c
+++ learning.c
@@ -8,7 +8,7 @@ int main()
       for(j=1; j<=n-1;j++){
          printf(" ");
       }
-      for(j=1; j<=(j*2)+1; j++){
+      for(j=1; j<=(i*2)+1; j++){
          printf("*");
       }
       printf("\n");
