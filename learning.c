f6871d581b5c7f1a121561f7275d177561e1371b
punke <kts300005@gmail.com>
2023-07-26 19:39:00 +0900

    problem 1358 try 1(infinity output)

--- learning.c
+++ learning.c
@@ -4,14 +4,11 @@ int main()
 {
    int i, j, n;
    scanf( "%d", &n );
-   for(i=1; i<n; i++){
-      for(j=1; j<=i; j++){
-         printf("*");
+   for(i=1;i<=n; i++){
+      for(j=1; j<=n-1;j++){
+         printf(" ");
       }
-      printf("\n");
-   }
-   for(i=n-1; i>=0; i--){
-      for(j=i; j>=0; j--){
+      for(j=1; j<=(j*2)+1; j++){
          printf("*");
       }
       printf("\n");
