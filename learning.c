4e34ab10eeb7c523b4e586a9d648f06efabfddfa
kimpunke <kts300005@gmail.com>
2023-08-01 15:46:47 +0900

    problem 1365 test 4

--- learning.c
+++ learning.c
@@ -4,13 +4,14 @@
 int main (){
     int n,i,j,k,count;
     scanf("%d", &n);
+    count=2;
     for(i=1; i<=n; i++){
       if(i%(n-1)==1){
          for(j=1; j<=n; j++){
             printf("*");
          }
       }
-      else if(i==(n/2)+1){
+      else if(n%2!=0&&i==(n/2)+1){
          for(j=1; j<=n; j++){
             if(j%(n-1)==1){
                printf("*");
@@ -23,6 +24,21 @@ int main (){
             }
          }
       }
+      else if(i<(n/2)+1){
+         for(j=1; j<=n; j++){
+            if(j==1||j==n){
+               printf("*");
+            }
+            else if(j==count||j==(n-count)+1){
+               printf("*");
+            }
+            else{
+               printf(" ");
+            }
+         }
+         count++;
+      }
+      
       printf("\n");
     }
     return 0;
