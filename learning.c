6aaa74655e70db202baf160f63f7aaca9b51d61e
kimpunke <kts300005@gmail.com>
2023-08-01 18:00:45 +0900

    problem 1368 try 1 (expression error)

--- learning.c
+++ learning.c
@@ -2,16 +2,31 @@
 
 
 int main (){
-    int i,n,j;
-    scanf("%d", &n);
-    for(i=1; i<=n; i++){
-      for(j=1; j<=n-i; j++){
-         printf(" ");
+    int i,j,height,weight,count;
+    char space;
+    scanf("%d %d %c", &height, &weight, &space);
+    if(space='R'){
+      for(i=1; i<=height; i++){
+         for(j=1; j<=height-i; j++){
+            printf(" ");
+          }
+         for(j=1; j<=weight; j++){
+            printf("*");
+         }
+         printf("\n");
       }
-      for(j=1; j<=n; j++){
-         printf("*");
+    }
+    else {
+      count = 1;
+      for(i=1; i<=height; i++){
+         for(j=1; j<count; j++){
+            printf(" ");
+         }
+         for(j=1; j<=weight; j++){
+            printf("*");
+         }
+         printf("\n");
       }
-      printf("\n");
     }
     return 0;
 }
