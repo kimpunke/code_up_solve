e7b02777ac4ac2662bdfe69ccd28c64b87eeed23
kimpunke <kts300005@gmail.com>
2023-08-01 18:05:52 +0900

    problem 1369 try 3 (answer)

--- learning.c
+++ learning.c
@@ -5,7 +5,7 @@ int main (){
     int i,j,height,weight,count;
     char space;
     scanf("%d %d %c", &height, &weight, &space);
-    if(space='R'){
+    if(space=='R'){
       for(i=1; i<=height; i++){
          for(j=1; j<=height-i; j++){
             printf(" ");
@@ -28,6 +28,7 @@ int main (){
          }
          if(i==height) return 0;
          printf("\n");
+         count++;
       }
     }
     return 0;
