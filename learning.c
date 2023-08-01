3141b9b1fc89745e0918a34f8aa521a83d85fd4c
kimpunke <kts300005@gmail.com>
2023-08-01 15:12:32 +0900

    problem 1365 test 1

--- learning.c
+++ learning.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
-int f(int a){
-   if(a==1) return 1;
-   else {
-      return a+f(a-1);
-   }
-}
+
 
 int main (){
-    int n,i,j,count;
+    int n,i,j,k,count;
     scanf("%d", &n);
-    count = f(n);
     for(i=1; i<=n; i++){
-      for(j=1; j<=i; j++){
-         printf("%d ", count);
-         count--;
+      if(i%(n-1)==1){
+         for(j=1; j<=n; j++){
+            printf("*");
+         }
       }
-      printf("\n");
+      else if(n%i==(i-1)){
+         for(j=1; j<=n; j++){
+            if(j%(n-1)==1){
+               printf("*");
+            }
+            else if(j==(n/2)+1){
+               printf("*");
+            }
+            else {
+               printf(" ");
+            }
+         }
+      }
     }
     return 0;
 }
