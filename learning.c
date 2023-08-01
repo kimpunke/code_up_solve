ea0931e084909a0910a3cdc8d73cc7796dd953bb
kimpunke <kts300005@gmail.com>
2023-08-01 17:49:24 +0900

    problem 1367 try 1 (answer)

--- learning.c
+++ learning.c
@@ -2,16 +2,14 @@
 
 
 int main (){
-    int m,n,i,j;
-    scanf("%d %d", &n, &m);
-    for(i=0; i<n; i++){
+    int i,n,j;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++){
+      for(j=1; j<=n-i; j++){
+         printf(" ");
+      }
       for(j=1; j<=n; j++){
-         if(i==0||i==n-1||(i+j)%m==0||j==1||j==n){
-            printf("*");
-         }
-         else{
-            printf(" ");
-         }
+         printf("*");
       }
       printf("\n");
     }
