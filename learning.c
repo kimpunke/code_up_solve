fa12999e37d3aee59d3dac922df56a709cdc4030
kimpunke <kts300005@gmail.com>
2023-08-01 17:37:36 +0900

    problem 1369 try 1

--- learning.c
+++ learning.c
@@ -2,57 +2,18 @@
 
 
 int main (){
-    int n,i,j,k,count;
-    scanf("%d", &n);
-    count=1;
+    int m,n,i,j;
+    scanf("%d %d", &n, &m);
     for(i=1; i<=n; i++){
-      if(i%(n-1)==1){
-         for(j=1; j<=n; j++){
+      for(j=1; j<=n; j++){
+         if(i==1||i==8||(i+j)%3==0||j==1||j==n){
             printf("*");
          }
-      }
-      else if(n%2!=0&&i==(n/2)+1){
-         for(j=1; j<=n; j++){
-            printf("*");
+         else{
+            printf(" ");
          }
+         printf("\n");
       }
-      else if(i<(n/2)+1){
-         count++;
-         for(j=1; j<=n; j++){
-            if(j==1||j==n){
-               printf("*");
-            }
-            else if(j==count||j==(n-count)+1){
-               printf("*");
-            }
-            else if(j==(n/2)+1){
-               printf("*");
-            }
-            else{
-               printf(" ");
-            }
-         }
-         
-      }
-      else{
-         for(j=1; j<=n; j++){
-            if(j==1||j==n){
-               printf("*");
-            }
-            else if(j==count||j==(n-count)+1){
-               printf("*");
-            }
-            else if(j==(n/2)+1){
-               printf("*");
-            }
-            else{
-               printf(" ");
-            }
-         }
-         count--;
-      }
-      
-      printf("\n");
     }
     return 0;
 }
