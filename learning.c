8f5f5a2b9cfeb0c5d6fd12ae020de65bf625f0ab
punke <kts300005@gmail.com>
2023-08-13 20:59:26 +0900

    problem 1153 try 1 (answer)

--- learning.c
+++ learning.c
@@ -2,38 +2,17 @@
 
 int main()
 {
-   int i,j,k,n,count=1,count2=0;
-   scanf( "%d", &n);
-   for(i=1; i<=n; i++){
-    for(j=1; j<=n-count; j++){
-        printf(" ");
-    }
-    printf("*");
-    for(j=1; j<=count2; j++){
-        printf(" ");
-    }
-    printf("*");
-    count++;
-    count2+=2;
-    printf("\n");
+   int a,b;
+   scanf("%d %d",&a,&b);
+   if(a>b){
+    printf(">");
    }
-   count2-=2;
-   count--;
-   for(i=1; i<=n; i++){
-    for(j=1; j<=n-count; j++){
-        printf(" ");
-    }
-    printf("*");
-    for(j=1; j<=count2; j++){
-        printf(" ");
-    }
-    printf("*");
-    count--;
-    count2-=2;
-    printf("\n");
+   else if(a<b){
+    printf("<");
    }
-   
-
+   else{
+    printf("=");
+   }
    
    return 0;
 }
