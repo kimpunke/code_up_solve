028d240a11afc38c94a46355d3646fbd729da9b4
kimpunke <kts300005@gmail.com>
2023-08-29 08:34:19 +0900

    problem 1168 try 2

--- learning.c
+++ learning.c
@@ -6,11 +6,11 @@ int main()
    scanf("%d %d",&birth,&year);
    if(year<=2){
     age=birth/10000;
-    printf("%d",2012-1900-age);
+    printf("%d",2012-1901-age);
    }
    else{
     age=birth/10000;
-    printf("%d",2012-2000-age);
+    printf("%d",2012-2001-age);
    }
 
    return 0;
